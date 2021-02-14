ea0651f0f62d582375082c44403d7b7b71027216
Hendi Santika <hendisantika@gmail.com>
2021-02-15 05:58:04 +0700

    #8 - Add save API in CustomerController

--- src/main/java/com/hendisantika/springbootcrudcustomerrestapimysql/controller/CustomerController.java
+++ src/main/java/com/hendisantika/springbootcrudcustomerrestapimysql/controller/CustomerController.java
@@ -1,9 +1,14 @@
 package com.hendisantika.springbootcrudcustomerrestapimysql.controller;
 
+import com.hendisantika.springbootcrudcustomerrestapimysql.entity.Customer;
 import com.hendisantika.springbootcrudcustomerrestapimysql.repository.CustomerRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,4 +26,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Customer save(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+
 }
