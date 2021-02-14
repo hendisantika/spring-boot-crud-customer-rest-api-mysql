3facc199d819b9d06066320d063d532c5763d597
Hendi Santika <hendisantika@gmail.com>
2021-02-15 05:57:44 +0700

    #7 - Add CustomerController

---
+++ src/main/java/com/hendisantika/springbootcrudcustomerrestapimysql/controller/CustomerController.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootcrudcustomerrestapimysql.controller;
+
+import com.hendisantika.springbootcrudcustomerrestapimysql.repository.CustomerRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-crud-customer-rest-api-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/02/21
+ * Time: 05.57
+ */
+@Log4j2
+@RestController
+@RequestMapping("api/customers")
+public class CustomerController {
+    @Autowired
+    private CustomerRepository customerRepository;
+}
